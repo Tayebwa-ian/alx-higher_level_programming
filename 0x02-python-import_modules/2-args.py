a98522cc5b374f579e69f05d5727ef047ce9bfb5
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-02 06:17:46 -0700

    Use command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ def main():
     num = len(argv)
     if num == 1:
         print("{} arguments.".format(num-1))
+    elif num == 2:
+        print("{} argument:".format(num-1))
     else:
         print("{} arguments:".format(num-1))
     for i in range(num):

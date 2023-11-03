adf64b88ca3f231684e263898896af1e0ab2f4d3
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-03 13:31:47 -0700

    remove charcters in a string

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,6 @@ def replace_in_list(my_list, idx, element):
     rn = len(my_list) - 1
     if idx < 0 or idx > rn:
         return my_list
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list[i] = element
+    else:
+        my_list[idx] = element
     return my_list

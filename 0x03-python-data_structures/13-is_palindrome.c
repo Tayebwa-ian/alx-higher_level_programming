9ecd59fe0832856978511cddd4abaa15a125079c
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-05 12:52:55 -0800

    Determine if linked list ins palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * list_len -  function that returns the number of elements in a linked list_t
+ *@h: the linked list
+ *Return: number of elements in a list
+ */
+size_t list_len(listint_t *h)
+{
+	int i = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
  *reverse_list - reverse a linked list
  *@head: head of the list
  *Return: address of reversed list or NULL
@@ -28,12 +48,16 @@ listint_t *reverse_list(listint_t *head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *rev, *current;
+	size_t len;
 
 	if (head== NULL)
 		return (0);
 	else if (*head == NULL)
 		return (1);
 	current = *head;
+	len = list_len(current);
+	if (len % 2 != 0)
+		return (0);
 	rev = reverse_list(current);
 	while (current != NULL && rev != NULL)
 	{

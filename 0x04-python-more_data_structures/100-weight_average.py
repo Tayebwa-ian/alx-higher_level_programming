6cba1e23c17be596d310d0437b0b32c248ddb5b9
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-08 10:29:48 -0800

    calculate the weighted average of all integers tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    num = 0
+    deno = 0
+    for i in my_list:
+        num += i[0] * i[1]
+        deno += i[1]
+    result = num/deno
+    return result

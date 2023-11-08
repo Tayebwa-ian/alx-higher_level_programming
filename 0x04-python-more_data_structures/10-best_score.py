a04b14492a84a7d6fc1e2f087a17ddd18dc31fa5
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-08 08:36:58 -0800

    find  key with the biggest integer value.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    max_key = ""
+    max_value = 0
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if value >= max_value:
+                max_key = key
+                max_value = value
+        return max_key
+    else:
+        return None

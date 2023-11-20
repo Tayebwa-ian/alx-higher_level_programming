b1735c53787d2aadd79baa644c5fbb0561902372
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-20 02:07:54 -0800

    print only integers in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             n += 1
         print()
-        return n
     except IndexError as er:
         print()
-        return n
+    return n
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print(my_list[i], end="")
+                n += 1
+            else:
+                continue
+        print()
+    except IndexError as er:
+        raise er
+    return n

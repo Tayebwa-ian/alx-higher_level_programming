72c62be3513f89c396b4db90bc051d2e080feb07
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-20 05:25:32 -0800

    excute a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as er:
+        print("Exception: {}".format(er), file = sys.stderr)
+        result = None
+    return result

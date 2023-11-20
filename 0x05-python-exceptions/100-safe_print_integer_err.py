24260bc0938664b01fdb7af42dffe87f296178a5
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-20 05:13:18 -0800

    print errors to the stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """
+    Description: prints integers
+    Params:
+     -value: any datatype
+    Return: True if it print the integer otherwise false
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as er:
+        sys.stderr.write("Exception: {}\n".format(er))
+        return False

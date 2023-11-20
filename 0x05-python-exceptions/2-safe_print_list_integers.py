64b6dc6f7603d21683412cb7abd1f180478c081b
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-20 02:20:13 -0800

    print integers only

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
     try:
@@ -13,3 +14,7 @@ def safe_print_list_integers(my_list=[], x=0):
     except IndexError as er:
         raise er
     return n
+
+
+if __name__ == "__main__":
+    safe_print_list_integers()

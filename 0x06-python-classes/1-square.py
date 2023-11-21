f4f029a7437e7c25853c7131b10e2c670ad83cf4
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-21 05:29:41 -0800

    create a square class with private instance variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """Perform square operations"""
+    def __init__(self, size):
+        """Initialisation of the square class
+
+        Args:
+          -size (int): length of the square
+        Returns: nothing
+        """
+        self.__size = size

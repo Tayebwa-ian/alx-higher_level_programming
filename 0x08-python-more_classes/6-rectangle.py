cc21ab507974ab60c39a4b92a6cf55ee3289032a
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-27 02:58:03 -0800

    calculating the number of instances of created and destroyed

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Perform operations on rectangles"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Intialises the class and calculates number of instances formed
         Args:

6ba9593b71e28293c8b811acdbc159b5d3389fef
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-27 01:28:11 -0800

    define getters and setters for height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+
+
+class Rectangle:
+    """Perform operations on rectangles"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Retrieve width value of the rectangle
+        Return: width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of rectangle width
+        Args:
+          -Value: value of the rectangle width
+        Raises:
+          -TypeError: if width is not a integer
+          -ValueError: if width is less than 0
+        Returns: None
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height value of the rectangle
+        Return: height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of rectangle height
+        Args:
+          -Value: value of the rectangle height
+        Raises:
+          -TypeError: if height is not a integer
+          -ValueError: if height is less than 0
+        Returns: None
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

2c6c0264408f69f920fa42c4e0db29e7eaab1db3
Tayebwa-ian <tayebwaian0@gmail.com>
2023-11-27 01:51:51 -0800

    find area and perimeter of a rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+
+
+class Rectangle:
+    """Perform operations on rectangles"""
+    def __init__(self, width=0, height=0):
+        """Constructor
+        Args:
+          -width: rectangle width
+          -height: rectangle height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width value of the rectangle
+        Return: width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of rectangle width
+        Args:
+          -Value: value of the rectangle width
+        Raises:
+          -TypeError: if width is not a integer
+          -ValueError: if width is less than 0
+        Returns: None
+        """
+        lst = [int, float]
+        if type(value) not in lst:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height value of the rectangle
+        Return: height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of rectangle height
+        Args:
+          -Value: value of the rectangle height
+        Raises:
+          -TypeError: if height is not a integer
+          -ValueError: if height is less than 0
+        Returns: None
+        """
+        lst = [int, float]
+        if type(value) not in lst:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate the area of a rectangle
+        Return: Area of the rectangle
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """calculate rectangle perimeter
+        Return: parameter if width of height is not zero and 0 otherwise
+        """
+        if self.height == 0 or self.width == 0:
+            return 0
+        return 2 * (self.height + self.width)

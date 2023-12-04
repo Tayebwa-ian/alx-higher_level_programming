81ce183275e55d258fc818cd20aba8864657c549
Tayebwa-ian <tayebwaian0@gmail.com>
2023-12-04 10:47:17 -0800

    Define square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from rectangle class"""
+    def __init__(self, size):
+        """Overrides the supper class init func to make it a square"""
+        super().__init__(size, size)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -24,5 +24,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """returns the string rep of the object"""
-        return("[{}] {}/{}".format(type(self).__name__,
-                                   self.__width, self.__height))
+        return("[Rectangle] {}/{}".format(self.__width, self.__height))

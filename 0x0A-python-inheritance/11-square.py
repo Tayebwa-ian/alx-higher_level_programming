834bbd7787b8255494bb25cd3afdc0163288bb4d
Tayebwa-ian <tayebwaian0@gmail.com>
2023-12-04 11:11:30 -0800

    define more of square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square Module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from rectangle class"""
+    def __init__(self, size):
+        """Overrides the supper class init func to make it a square"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calaculate the area of a rectangle
+        Return: area
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """returns the string rep of the object"""
+        return("[{}] {}/{}".format(type(self).__name__,
+                                   self.__size, self.__size))

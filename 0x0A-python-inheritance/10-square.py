27a79712f686f1524318758494be165cf6f5e653
Tayebwa-ian <tayebwaian0@gmail.com>
2023-12-04 11:00:28 -0800

    Define square class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,4 +7,12 @@ class Square(Rectangle):
     """Square class that inherits from rectangle class"""
     def __init__(self, size):
         """Overrides the supper class init func to make it a square"""
+        self.integer_validator('size', size)
+        self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        """Calaculate the area of a rectangle
+        Return: area
+        """
+        return self.__size * 2

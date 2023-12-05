a698fab5ec1e823cf7e808b8bc33dd3ae989fb1e
Tayebwa-ian <tayebwaian0@gmail.com>
2023-12-05 09:54:04 -0800

    add all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 if __name__ == "__main__":
     try:
         obj = load_from_json_file("add_item.json")
-    except:
+    except FileNotFoundError as e:
         obj = None
     if obj is not None:
         for arg in sys.argv[1:]:

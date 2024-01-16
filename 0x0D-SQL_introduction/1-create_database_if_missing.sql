0f79dda058c6145df2a6e503abf1fe64508c6f1d
Tayebwa-ian <tayebwaian0@gmail.com>
2024-01-16 10:53:23 -0800

    create and delete a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0c_0
+-- SQL statement to create the table
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0
+-- sql statement that deletes a database
+DROP DATABASE IF EXISTS hbtn_0c_0;

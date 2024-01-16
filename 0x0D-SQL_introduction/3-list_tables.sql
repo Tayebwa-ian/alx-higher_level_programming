4e4c8a8d0811b2fe35358eb2a5a4114202c9b163
Tayebwa-ian <tayebwaian0@gmail.com>
2024-01-16 13:55:36 -0800

    working with tables

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Lists all the tables of a database
+-- sql statement that lists all tables in a database
+SHOW TABLES
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- create a table in the current database
+-- SQL statement to create a table
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table(
+       id INT,
+       name VARCHAR(256)
+);

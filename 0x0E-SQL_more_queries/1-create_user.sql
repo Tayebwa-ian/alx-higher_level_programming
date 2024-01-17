3f8de7f35ce7925cfebebabab34c7ef82e33406f
Tayebwa-ian <tayebwaian0@gmail.com>
2024-01-17 09:26:19 -0800

    create and give priviliges to database users

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant the user all priviliges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

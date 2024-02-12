61f6ac988509d80886890f57746f7493313de8af
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-12 13:00:59 -0800

    using loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-let len = 0;
-argv.forEach((val, index) => len++);
-if (len < 3) {
-  console.log('Missing number of occurrences');
-} else if (isNaN(Number(argv[2]))) {
+if (isNaN(Number(argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < Number(argv[2]); i++) {

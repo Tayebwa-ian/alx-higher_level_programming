f8675d61bdf4acb0c56d4214aac0558e44f20db2
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-12 15:29:33 -0800

    get the seconf largest number in the given list of number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,12 @@
 
 const argv = process.argv;
 let largest, seclargest;
-if (isNaN(Number(argv[2])) || argv.length === 3) {
-  console.log(0);
-} else {
-  largest = argv[2];
-  for (let i = 2; i < argv.length; i++) {
-    if (argv[i] > largest) {
-      seclargest = largest;
-      largest = argv[i];
-    }
+largest = argv[2];
+seclargest = 0;
+for (let i = 2; i < argv.length; i++) {
+  if (argv[i] > largest) {
+    seclargest = largest;
+    largest = argv[i];
   }
-  console.log(seclargest);
 }
+console.log(seclargest);

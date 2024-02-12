c5ee1f08082b267fd0152722cfa00cb0273d349b
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-12 11:42:47 -0800

    Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+let len = 0;
+argv.forEach((val, index)=> len++);
+if (len < 3) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}

45e3b88b125e6c3694fb7d62c197fe3fdcc5d422
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-13 12:03:40 -0800

    Inheritance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let ch = '';
+        for (let j = 0; j < this.width; j++) {
+          ch += c;
+        }
+        console.log(ch);
+      }
+    }
+  }
+}
+module.exports = Square;

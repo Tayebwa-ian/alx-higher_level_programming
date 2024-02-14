add059b6ab93c1f326f57ef7c68d76f0de381f3f
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-14 07:35:26 -0800

    find second largest number in a list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 
-const argv = process.argv;
+const argv = process.argv.map((el, _idx) => Number(el));
 let largest, seclargest;
 largest = argv[2];
 seclargest = 0;
 for (let i = 2; i < argv.length; i++) {
-  if (argv[i] > largest) {
-    seclargest = largest;
-    largest = argv[i];
-  } else if (argv[i] > seclargest) {
-    seclargest = argv[i];
-  }
+    if (argv[i] >= largest) {
+	seclargest = largest;
+	largest = argv[i];
+	for (let j = 2; j < argv.length; j++) {
+	    if (argv[j] > seclargest && argv[j] != argv[i]) {
+		seclargest = argv[j];
+	    }
+	}
+    }
 }
 console.log(seclargest);

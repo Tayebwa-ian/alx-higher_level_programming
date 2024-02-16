dd468a1b2ea4a595fce8dd7db4bd14b16882b825
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-16 14:06:58 -0800

    Make connection to the databse using MySQLdb module

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, database=db_name,
+                         user=username, password=password)
+    cursor = db.cursor()
+
+    cursor.execute("""SELECT * FROM states ORDER BY name ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()

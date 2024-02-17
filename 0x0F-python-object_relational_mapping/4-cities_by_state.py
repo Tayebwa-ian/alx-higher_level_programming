77e6d3282d33ac262e0a5a36c9b63a84715b8307
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-17 13:22:18 -0800

    List all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Make a connectiion to the database using the MySQldb module"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, database=db_name,
+                         user=username, password=password)
+    cursor = db.cursor()
+
+    cursor.execute("""SELECT * FROM cities ORDER BY id ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()

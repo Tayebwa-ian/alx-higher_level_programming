9663a5881e8d93e0deceb0557841b87149de753d
Tayebwa-ian <tayebwaian0@gmail.com>
2024-02-17 12:21:55 -0800

    using format function to creat an SQL query

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Make a connectiion to the database using the MySQldb module"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    search_name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, database=db_name,
+                         user=username, password=password)
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+
+    cursor.execute(query.format(search_name))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()

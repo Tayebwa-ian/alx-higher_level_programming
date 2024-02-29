1049f81de63ae4d451f05769caf5faab4f3fb53a
Tayebwa-ian <tayebwaian0@gmail.com>
2024-03-01 01:46:08 +0300

    Find the peak in the list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,18 +8,22 @@ def find_peak(list_of_integers) -> int:
         list_of_integers: a list of unsorted integers
     Return: peak value or None
     """
-    if len(list_of_integers) > 0:
+    length = len(list_of_integers)
+    if length > 0:
         peak = list_of_integers[0]
-        temp = list_of_integers[0]
         i = 0
         j = -1
-        m = len(list_of_integers) // 2
-        while i < m:
-            temp = list_of_integers[i]
-            if list_of_integers[i] < list_of_integers[j]:
-                temp = list_of_integers[j]
-            if temp > peak:
-                peak = temp
-            i += 1
-            j -= 1
+        if length == 2 and list_of_integers[-1] > peak:
+            peak = list_of_integers[-1]
+        elif length > 2:
+            m = length // 2
+            temp = list_of_integers[0]
+            while i < m:
+                temp = list_of_integers[i]
+                if list_of_integers[i] < list_of_integers[j]:
+                    temp = list_of_integers[j]
+                if temp > peak:
+                    peak = temp
+                i += 1
+                j -= 1
         return(peak)

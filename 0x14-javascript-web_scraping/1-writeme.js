c532fe58d7a5a9352d5a84fffa40bf0f1908cbb3
Tayebwa-ian <tayebwaian0@gmail.com>
2024-03-26 23:08:13 +0300

    write to a file using js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 
 const filename = process.argv.slice(2)[0];
 const input = process.argv.slice(2)[1];
-console.log(filename);
 fs.writeFile(filename, input, (e) => {
   if (e) {
     console.log(e);

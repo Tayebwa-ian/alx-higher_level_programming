256a8f629511d53759997a0b60a84140eb738561
Tayebwa-ian <tayebwaian0@gmail.com>
2024-03-27 01:36:48 +0300

    films and characters

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// fetch and use data from Star wars API
+const request = require('request');
+
+const url = process.argv.slice(2)[0];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const content = JSON.parse(body);
+    const films = content.results;
+    let count = 0;
+    for (const i in films) {
+      // collection of all film characters
+      const cast = films[i].characters;
+      if (cast.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count += 1;
+      }
+    }
+    console.log(count);
+  }
+});
